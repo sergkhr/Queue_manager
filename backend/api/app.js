17de0d187bb58c59622ea5c1d9dc5c3daba39e03
MrNightFury <MrCreeperCowboy@yandex.ru>
2023-03-07 21:08:18 +0300

    Добавил хендлер для админ-команд и настроил по-человечески сервер

--- backend/api/app.js
+++ backend/api/app.js
@@ -7,21 +7,30 @@ export default class Application {
         this.expressApp = Express();
         this.manager = new QueueManager();
         this.setupHandlers();
+        let app = this.expressApp;
+        app.use(bodyParser.json());
+        app.use(function(req, res, next) {
+            res.header("Access-Control-Allow-Origin", "http://127.0.0.1:5500");
+            res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+            next();
+        });
     }
     start(config) {
         this.listener = this.expressApp.listen(config.port, config.host, function() {
             console.log(`App listening at port ${config.port}`);
         });
     }
+    setupHandlers() {
+        let app = this.expressApp;
+
+        app.get('/rooms', this.getTest.bind(this));
+        app.post('/rooms', this.postTest.bind(this));
+        app.post('/admin', this.adminPanelHandler.bind(this));
+    }
     save() {
         console.log("Saving...");
     }
-    setupHandlers() {
-        let app = this.expressApp;
-        let jsonParser = bodyParser.json();
-        app.get('/rooms', this.getTest.bind(this));
-        app.post('/rooms', jsonParser, this.postTest.bind(this));
-    }
+    
     getTest(req, res) {
         let searchString = req.query.searchString || '';
         console.log(`GET: ${toString(req.query)}`);
@@ -32,6 +41,16 @@ export default class Application {
         res.json({text: req.body});
         this.listener.close(()=>{
             console.log("Text");
+        });
+    }
+
+    adminPanelHandler(req, res) {
+        console.log("Admin command got: " + JSON.stringify(req.body) + " " + req.command);
+        // for (let key in req) {
+        //     console.log(key + "-" + JSON.stringify(req[key]));
+        // }
+        res.json({
+            text: "asd"
         });
     }
 }
