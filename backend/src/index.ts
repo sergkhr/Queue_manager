05b0a9d2fecc83fa39e9d7dec812c5aec107356c
MrNightFury <MrCreeperCowboy@yandex.ru>
2023-03-23 13:41:21 +0300

    Добавил линтер

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,11 +1,10 @@
-import Application from "./api/app.js";
-// import config from "../config.json"
+import Application from "./api/app.js"
 import process from "node:process"
 import fs from "node:fs"
 
 let config = JSON.parse(fs.readFileSync("config.json", "utf8"));
 
-let app = new Application();
+var app = new Application();
 app.start(config);
 
 process.on('SIGINT', () => {
