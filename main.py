182c0b7dff52f9775d8545960909504708869985
Timur_Komolov <100406268+Timik232@users.noreply.github.com>
2023-04-04 13:02:38 +0300

    fix closing due to errors

--- main.py
+++ main.py
@@ -245,3 +245,5 @@ if __name__ == "__main__":
         except requests.exceptions.ReadTimeout:
             print("readtimeout")
             time.sleep(600)
+        except Exception as ex:
+            print(ex)
