f84415c34df8dab4b9313e43bbd09889f75fe7c7
MrNightFury <73826973+MrNightFury@users.noreply.github.com>
2023-04-22 21:34:57 +0300

    Упоролся и сделал третью часть проекта

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -5,12 +5,20 @@ import { MongoClient } from "mongodb"
 
 let config = JSON.parse(fs.readFileSync("config.json", "utf8")) as AppConfig;
 
-console.log("Connecting to db...");
+console.log("Reading enviroment variables...");
+config.db.host = process.env.DB_HOST || config.db.host;
+config.db.port = Number(process.env.DB_PORT) || config.db.port;
 
+console.log("Final config:")
+console.log(JSON.stringify(config));
+
+console.log("Connecting to db...");
 const dbClient: MongoClient = await (MongoClient.connect("mongodb://" + config.db.host + ":" + config.db.port).catch(err => {
     console.log(err);
     process.abort();
 }));
+
+console.log("Starting application");
 var app = new Application(dbClient, config);
 app.start();
 
