153e95bf341444e44d3d9fac0b1fc22abd28e706
sergkhr <serg.khrechko@gmail.com>
2023-06-01 13:30:19 +0300

    made getting queues and creating queue

---
+++ FrontEnd/scripts/addQueueScript.js
@@ -0,0 +1,34 @@
+function createQueue() {
+    // Get the form values
+    let name = document.getElementById('name').value;
+    let description = document.getElementById('description').value;
+
+    // Create the request body
+    let requestBody = {
+      command: "create",
+      arguments: {
+        name: name,
+        description: description
+      }
+    };
+
+    // Send the PUT request
+    fetch(ip + '/queues', {
+      method: 'post',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify(requestBody)
+    })
+    .then(function(response) {
+      if (response.ok) {
+        console.log('Queue created successfully!');
+      } else {
+        alert('Error creating queue. Please try again.');
+      }
+    })
+    .catch(function(error) {
+      alert('Error creating queue. Please try again.');
+      console.error(error);
+    });
+  }
